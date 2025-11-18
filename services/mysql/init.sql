13889b8b587592f592b67202ec784cd1cf4e95fa
Chinh <chinhtv9820@ut.edu.vn>
2025-11-18 23:41:52 +0700

    tạo csdl lưu trữ

---
+++ services/mysql/init.sql
@@ -0,0 +1,8 @@
+-- services/mysql-init/init.sql
+CREATE DATABASE IF NOT EXISTS edumathdb;
+CREATE DATABASE IF NOT EXISTS edumatch_applications_db;
+CREATE DATABASE IF NOT EXISTS edumatch_users_db;
+
+-- Đảm bảo user có quyền (phòng hờ)
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
+FLUSH PRIVILEGES;
