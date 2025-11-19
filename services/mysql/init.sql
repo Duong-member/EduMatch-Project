7e6d1e4a08babec46305112d8444e561a0e69c59
Chinh <chinhtv9820@ut.edu.vn>
2025-11-19 11:54:57 +0700

    thêm bảng cho student

--- services/mysql/init.sql
+++ services/mysql/init.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS edumathdb;
 CREATE DATABASE IF NOT EXISTS edumatch_applications_db;
 CREATE DATABASE IF NOT EXISTS edumatch_users_db;
-
+CREATE DATABASE IF NOT EXISTS student_profile_db;
 -- Đảm bảo user có quyền (phòng hờ)
 GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
 FLUSH PRIVILEGES;
