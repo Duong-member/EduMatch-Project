e5d735d4454f51323da3c2c576d8a2c32a727cf0
Chinh <chinhtv9820@ut.edu.vn>
2025-11-20 10:45:39 +0700

    fix

--- services/mysql-init/init.sql
+++ services/mysql-init/init.sql
@@ -2,8 +2,6 @@
 CREATE DATABASE IF NOT EXISTS edumathdb;
 CREATE DATABASE IF NOT EXISTS edumatch_applications_db;
 CREATE DATABASE IF NOT EXISTS edumatch_users_db;
-CREATE DATABASE IF NOT EXISTS student_profile_db;
--- ➕ Database cho Chat Service
 CREATE DATABASE IF NOT EXISTS edumatch_chat_db;
 CREATE DATABASE IF NOT EXISTS edumatch_student_db; 
 CREATE DATABASE IF NOT EXISTS edumatch_admin_db;
